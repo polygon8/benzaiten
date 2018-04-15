8a98f4b6546a14cae1cdc87fc3094c34c656255a
brenetic <b@brenetic.com>
2018-04-15 13:02:49 +0100

    Edit pseudonyms
    We can now add, edit and delete pseudonyms

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
-from .models import CustomUser
+from .models import CustomUser, Pseudonym
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -13,3 +14,9 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = UserChangeForm.Meta.fields
+
+
+class PseudonymChangeForm(forms.ModelForm):
+    class Meta:
+        model = Pseudonym
+        fields = ('name',)
--- users/views.py
+++ users/views.py
@@ -1,10 +1,10 @@
-from django.urls import reverse_lazy
+from django.urls import reverse, reverse_lazy
 from django.views import generic
 from django.shortcuts import get_object_or_404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .forms import CustomUserCreationForm
-from .models import CustomUser
+from .models import CustomUser, Pseudonym
 
 
 def profile(request, user_id):
@@ -22,6 +22,30 @@ def profile(request, user_id):
     )
 
 
+def pseudonyms(request, user_id):
+    user = get_object_or_404(CustomUser, pk=user_id)
+
+    if request.method == 'GET':
+        pseudonyms = user.pseudonym_set.all()
+        return render(
+            request,
+            'account/pseudonyms.html',
+            {'pseudonyms': pseudonyms}
+        )
+    elif request.method == 'POST':
+        Pseudonym.objects.create(
+            name=request.POST.get('name'),
+            user=user
+        )
+        return redirect(reverse('profile', kwargs={'user_id': user.id}))
+
+
+def delete_pseudonym(request, pseudonym_id):
+    pseudonym = get_object_or_404(Pseudonym, pk=pseudonym_id)
+    pseudonym.delete()
+    return redirect(reverse('profile', kwargs={'user_id': pseudonym.user.id}))
+
+
 class SignUp(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
@@ -32,3 +56,9 @@ class CustomUserChangeForm(generic.UpdateView):
     model = CustomUser
     fields = ['name', 'email']
     template_name = 'account/edit_profile.html'
+
+
+class PseudonymChangeForm(generic.UpdateView):
+    model = Pseudonym
+    fields = ['name']
+    template_name = 'account/edit_pseudonym.html'
