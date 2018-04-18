4eb5ecdaf62470c59fe97622f1c78f866512f952
brenetic <b@brenetic.com>
2018-04-18 20:45:10 +0100

    Add, edit and delete tracks
    Users can now add info about a track. Uploading of files to come

--- media/urls.py
+++ media/urls.py
@@ -1,7 +1,18 @@
-from django.urls import path # noqa
+from django.urls import path
 
-from . import views # noqa
+from . import views
 
 urlpatterns = [
-
+    path('tracks/', views.tracks, name='tracks'),
+    path('track/add', views.add_track, name='add-track'),
+    path(
+        'track/<int:pk>/edit',
+        views.TrackChangeForm.as_view(),
+        name='edit-track'
+    ),
+    path(
+        'track/<int:track_id>/delete',
+        views.delete_track,
+        name='delete-track'
+    )
 ]
--- media/views.py
+++ media/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render # noqa
+from django.shortcuts import (
+    get_object_or_404,
+    redirect,
+    render
+)
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
+from .models import Track
+from .forms import TrackCreationForm
+
+
+def tracks(request):
+    tracks = Track.objects.filter(user=request.user)
+    display_name = request.user.name if request.user.name else request.user.email.split('@')[0] # noqa
+
+    return render(
+        request,
+        'media/tracks.html',
+        {
+            'tracks': tracks,
+            'display_name': display_name
+        }
+    )
+
+
+def add_track(request):
+    if request.method == 'GET':
+        return render(
+            request,
+            'media/track.html',
+            {'form': TrackCreationForm()}
+        )
+    elif request.method == 'POST':
+        form = TrackCreationForm(request.POST)
+
+        if form.is_valid():
+            track = form.save(commit=False)
+            track.user = request.user
+            track.save()
+            return redirect(reverse('tracks'))
+
+        return render(
+            request,
+            'media/track.html',
+            {'form': form}
+        )
+
+
+def delete_track(request, track_id):
+    track = get_object_or_404(Track, pk=track_id)
+    track.delete()
+    return redirect(reverse('tracks'))
+
+
+class TrackChangeForm(generic.UpdateView):
+    model = Track
+    fields = [
+        'title',
+        'description',
+        'year',
+        'duration',
+        'bpm'
+    ]
+    template_name = 'media/edit_track.html'
